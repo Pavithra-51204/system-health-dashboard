eaced00be8f007761d4aea8f84d73ff013957ecb
Pavithra Manogaran <pavithra.manogaran2004@gmail.com>
2026-05-14 21:46:28 +0530

    fix: add SSL to RDS PostgreSQL connection

--- app/backend/index.js
+++ app/backend/index.js
@@ -9,17 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const pool = new Pool({
-
   host: process.env.DB_HOST,
-
   port: process.env.DB_PORT || 5432,
-
   database: process.env.DB_NAME,
-
   user: process.env.DB_USER,
-
   password: process.env.DB_PASSWORD,
-
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 const cors = require('cors');
